17ed77cc974c889655f8d2c1afcfdbb46ab94768
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-22 06:20:34 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main - this will be exectude fast
+ * Mystr: this variable
+ * return: return 0 for success
+ */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 	return (0);
