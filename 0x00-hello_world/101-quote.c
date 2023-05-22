f30eacb4940d612372cde0f6801135443a59300b
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-22 08:52:28 -0700

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - This is point of antry of program
+ *Return: 1 for success
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
