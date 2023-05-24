f18f4733596a861fd22be81fb32977d0769cd902
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-24 08:12:06 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 successs
+ */
+int main(void)
+{
+	int numb = 112;
+
+	while (numb > 96)
+	{
+		putchar(numb);
+		numb--;
+
+	}
+	putchar('\n');
+	return (0);
+}
