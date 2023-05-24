9d7c13279bc1a8550cf2fbd7d0531b3d4a7e5de8
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-24 05:10:15 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of prograam
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int numb = 97;
+
+	while (numb > 123)
+	{
+		putchar (numb);
+		putchar ('\n');
+
+	}
+	return (0);
+}
+
