471c9e117b837e4438001ef8b911bf98a09e81f3
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-24 07:11:53 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: 0(success)
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 97; numb < 123; numb++)
+	{
+		putchar(numb); }
+	putchar('\n');
+	return (0); }
