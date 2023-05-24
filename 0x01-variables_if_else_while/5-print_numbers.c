926657bc5d05ce92a516784e86c6b785bc7c2969
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-24 07:45:01 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	int numb = 48;
+
+	while (numb < 58)
+	{
+		putchar(numb);
+		numb++;
+
+	}
+	putchar('\n');
+	return (0);
+
+}
