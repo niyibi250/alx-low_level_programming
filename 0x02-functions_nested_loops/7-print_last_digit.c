c93b12c7b452ab5664638e64c843c8d5baaa9f87
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-25 07:33:51 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - find last digit
+ * @c: number  of compute
+ * Return: last number
+ */
+int print_last_digit(int c)
+{
+	int last;
+
+	last = c % 10;
+	return (last);
+}
