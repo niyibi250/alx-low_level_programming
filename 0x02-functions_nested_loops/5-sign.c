9ae3c778116b65fc2ff152209128261a37bc868e
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-25 06:58:38 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print sign of number
+ * @n: parameter
+ * Return: 1 positive -1 negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
