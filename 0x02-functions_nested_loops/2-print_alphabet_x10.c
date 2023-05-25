6ffe987cb8f695ae04a2b85b19a3e8c75bf51fd9
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-25 05:40:49 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print letters
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int numb = 0;
+
+	int letter;
+
+	while (numb < 10)
+	{
+		for (letter = 97; letter < 123; letter++)
+		{
+			_putchar(letter);
+		}
+		numb++;
+	}
+	_putchar('\n');
+}
