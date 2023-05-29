c293416d9e1fcb758e78a3d2552ebdcbbfeb559b
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-29 06:56:47 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s: inputed string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < s[i]; i++)
+	{
+		continue;
+	}
+	return (i)
+}
