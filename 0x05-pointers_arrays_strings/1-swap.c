4805e718c0c56d01690d59c19b20166571c1dd80
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-29 04:16:53 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - This swap two numbers
+ * @a: this is first number
+ * @b: this secand number
+ */
+void swap_int(int *a, int *b)
+{
+	int *p = &a;
+
+	int *pp = &b;
+
+	p = &b;
+	pp = &a;
+}
+
+
