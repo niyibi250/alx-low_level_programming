5b0915648c80b8f67b20b08e6f7dac4aa61d735e
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-30 04:07:41 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - revarse string
+ * @s: string to revarse
+ */
+void rev_string(char *s)
+{
+	int i, x, temp;
+
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	len = len + i;
+	for (x = 0; x < (len / 2); x++)
+	{
+		temp = s[x];
+		s[i] = s[x - 1 - x];
+		s[x - 1 - x] = temp;
+	}
+}
