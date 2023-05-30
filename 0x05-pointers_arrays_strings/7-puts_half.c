731b69e86deade50558450647abee23b7edf9bd3
niyibi250 <ericniyibizi1998@gmail.com>
2023-05-30 06:02:04 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,14 +5,19 @@
  */
 void puts_half(char *str)
 {
-	int i, x, len = 0;
+	int i, x, n, len = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		continue;
 	}
 	len = len + i;
-	for (x = (len / 2); x < len; x++)
+	n = len / 2;
+	if ((len % 2) == 1)
+	{
+		n++;
+	}
+	for (x = n; x < len; x++)
 	{
 		_putchar(str[x]);
 	}
