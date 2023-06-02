a79e4ff0facfd28b726d93c8ff480caa7d942c0c
niyibi250 <ericniyibizi1998@gmail.com>
2023-06-02 05:18:58 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2;
-
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
 		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-	if (len1 == len2)
-		return (len1 - len2);
-	if (len1 < len2)
-		return (len1 - len2);
-	else
-		return (len1 - len2);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	return (*s1 - *s2);
 }
