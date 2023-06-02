e488d206d998cf1c1758fd402de4399104909370
niyibi250 <ericniyibizi1998@gmail.com>
2023-06-02 05:04:19 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compele two sentence
+ * @s1: first sentence
+ * @s2:secand sentence
+ * Return: 0 ,-1 and  1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int len1, len2;
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	if (len1 == len2)
+		return (0);
+	if (len1 < len2)
+		return (-1);
+	else
+		return (1);
+}
