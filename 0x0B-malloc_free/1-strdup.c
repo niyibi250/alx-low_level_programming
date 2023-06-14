73f52112f5201cb4e27e1cc5517a39b9b0b07dba
niyibi250 <ericniyibizi1998@gmail.com>
2023-06-14 13:16:46 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate
+ * @str: string
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr, *ret;
+
+	if (!str)
+		return (NULL);
+
+	ptr = str;
+	while (*ptr++)
+		size++;
+
+	ret = malloc(size + 1);
+	if (!ret)
+		return (NULL);
+
+	ptr = ret;
+	while (*str)
+		*ptr++ = *str++;
+
+	*ptr = 0;
+	return (ret);
+}
