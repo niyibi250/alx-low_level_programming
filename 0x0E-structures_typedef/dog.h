31e6c05026a0db105efa6bce58af19be6e700286
niyibi250 <ericniyibizi1998@gmail.com>
2023-06-21 10:48:24 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#ifndef DOGH
+#define DOGH
 /**
- * main -print 
- * Return:int
+ * struct dog - a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
+	float age;
 	char *owner;
-	int age;
-};
+} dog;
 
+typedef dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+int _putchar(char c);
+
+#endif
