0346833e9e4280623c4a004639764037ba7df3fb
niyibi250 <ericniyibizi1998@gmail.com>
2023-06-23 13:55:34 +0200

    task1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - this function add the given number
+ * @n: number of passed arguments
+ * Return: sum of number if n=0 return 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list page;
+	
+	int sum = 0;
+
+	unsigned int i;
+
+	va_start(page, n);
+	
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(page, int);
+	}
+	return (sum);
+}
