3ac1691d1e467d3337fa96ad28a1f337cdc6eefa
niyibi250 <ericniyibizi1998@gmail.com>
2023-06-23 07:59:44 -0700

    task1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,18 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list page;
-	
+
 	int sum = 0;
 
 	unsigned int i;
 
 	va_start(page, n);
-	
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
 	{
 		sum = sum + va_arg(page, int);
 	}
+	va_end(page);
 	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,6 @@
  * print_strings - print string
  * @separator:between strings
  * @n:number of strings
- * Return: void
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -19,10 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		word = va_arg(list, char*);
-		(word != NULL) ? printf("%s",word) : printf("(nil)");
+		(word != NULL) ? printf("%s", word) : printf("(nil)");
 		if (separator != NULL && i < (n - 1))
-			printf("%s",separator);
+			printf("%s", separator);
 	}
 	printf("\n");
-	return;
+	va_end(list);
 }
