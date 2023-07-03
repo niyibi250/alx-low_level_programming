84fa42a51ec37fcffcded1ea8d70923a68da736f
niyibi250 <ericniyibizi1998@gmail.com>
2023-07-03 13:44:55 -0700

    task01

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #define MAIN
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
