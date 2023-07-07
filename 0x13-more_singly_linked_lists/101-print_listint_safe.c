276d00f4ce4be0c6dfd4155e6bed31d7d0ae0ca2
niyibi250 <ericniyibizi1998@gmail.com>
2023-07-07 04:39:12 -0700

    task101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,15 +7,15 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
-	const listint_t *aux_node = head;
+	const listint_t *before_node = head;
 
 	if (!head)
 		exit(98);
 
-	while (aux_node)
+	while (before_node)
 	{
-		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
-		aux_node = aux_node->next;
+		printf("[%p] %i\n", (void *)before_node, before_node->n);
+		before_node = before_node->next;
 		i++;
 	}
 	return (i);
