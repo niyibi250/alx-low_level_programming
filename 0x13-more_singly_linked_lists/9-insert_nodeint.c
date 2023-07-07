a8d2ba688994bf72e12bcfae4dc953398a4273e4
niyibi250 <ericniyibizi1998@gmail.com>
2023-07-07 04:01:10 -0700

    task09&10

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - add element at any at any position
+ * @idx:position
+ * @n:data to add to node
+ * Return:address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *aux_node = *head;
+	listint_t *new_node;
+	unsigned int index;
+	unsigned int cont = 0;
+
+	/* create node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	/* border case for insert at the beginning */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
+
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->next = aux_node->next;
+		aux_node->next = new_node;
+		return (new_node);
+	}
+
+	free(new_node);
+	return (NULL);
+}
