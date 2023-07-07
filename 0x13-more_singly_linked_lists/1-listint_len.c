9294c36cb4d111479cc83896a33ee835287aa9d8
niyibi250 <ericniyibizi1998@gmail.com>
2023-07-07 02:28:49 -0700

    00task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *node = h;
+	size_t cont = 0;
+
+	while (new)
+	{
+		i++;
+		new = new->next;
+	}
+
+	return (i);
+}
