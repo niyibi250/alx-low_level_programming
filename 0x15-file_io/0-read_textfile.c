de20bfe84142a7315f148dc458ee88c976a1e714
eric <ericniyibizi1998@gmail.com>
2023-09-08 11:23:49 +0200

    file handling

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include <main.h>
+
+/**
+ * read_textfile - prints text
+ *
+ * @filename: file
+ * @letters: number of characters
+ *
+ * Return: actual number of letters read, 0 if end of file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int length, wrotechars;
+	char *buf;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
+		return (0);
+	}
+
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
+	free(buf);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
+}
